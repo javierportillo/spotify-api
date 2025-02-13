7c8a77d67700ddd35086eadc1a2e9e5b2a43a516
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2025-02-12 22:21:21 -0600

    adds the spotify custom provider

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,4 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 export default {
   /**
@@ -7,7 +7,44 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register({ strapi }: { strapi: Core.Strapi }) {
+    strapi
+      .plugin("users-permissions")
+      .service("providers-registry")
+      .add("spotify", {
+        icon: "https://cdn-icons-png.flaticon.com/512/2111/2111624.png",
+        enabled: true,
+        grantConfig: {
+          key: process.env.SPOTIFY_CLIENT_ID,
+          secret: process.env.SPOTIFY_CLIENT_SECRET,
+          // callback: `${strapi.config.server.url}/api/auth/spotify/callback`,
+          callback: "/spotify-callback.html",
+          scope: ["user-read-email", "user-library-read"],
+          authorize_url: "https://accounts.spotify.com/authorize",
+          access_url: "https://accounts.spotify.com/api/token",
+          oauth: 2,
+        },
+        async authCallback({ accessToken }) {
+          console.log("got the callback!");
+          const res = await fetch("https://api.spotify.com/v1/me", {
+            headers: { Authorization: `Bearer ${accessToken}` },
+          });
+
+          const profile = (await res.json()) as {
+            id: string;
+            email: string;
+            display_name: string;
+          };
+
+          return {
+            email: profile.email,
+            username: profile.display_name || profile.id,
+            spotifyId: profile.id,
+            provider: "spotify",
+          };
+        },
+      });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
